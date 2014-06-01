8cc6340cef75cae3be3de99827db1a1d7a98cf7b
zizu <zizu@zizu-Lenovo-B570.(none)>
2014-06-01 23:35:24 +0400

    added calculate finction

--- calc.c
+++ calc.c
@@ -154,3 +154,62 @@ module_init(sysfsexample_module_init);
 module_exit(sysfsexample_module_exit);
 MODULE_LICENSE("GPL");
 
+
+#else
+
+struct proc_dir_entry *calc_dir;
+struct proc_dir_entry *arg1;
+struct proc_dir_entry *arg2;
+struct proc_dir_entry *operation;
+struct proc_dir_entry *result;
+
+/*
+ * arg1 write handler
+ */
+int write_arg1(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(arg1_input, buf, count);
+	return count;
+}
+
+/*
+ * arg2 write handler
+ */
+int write_arg2(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(arg2_input, buf, count);
+	return count;
+}
+
+/*
+ * operation write handler
+ */
+int write_operation(struct file *file, const char *buf, unsigned long count, void *data)
+{
+	if(count > WRITE_SIZE) {
+		count = WRITE_SIZE;
+	}
+
+	memcpy(operation_input, buf, count);
+	return count;
+}
+
+/*
+ * result read handler
+ */
+int read_result(char *buffer, char **buffer_location,
+				  off_t offset, int buffer_length, int *eof, void *data)
+{
+	long res = calculate();
+
+	return sprintf(buffer, "%ld\n", res);
+}
+
