1c033aae6cb5a187dfe015697bc85242ab694872
zizu <zizu@zizu-Lenovo-B570.(none)>
2014-06-01 23:37:11 +0400

    added sysfs calculate cause

--- calc.c
+++ calc.c
@@ -213,3 +213,58 @@ int read_result(char *buffer, char **buffer_location,
 	return sprintf(buffer, "%ld\n", res);
 }
 
+int init_module()
+{
+	// parent dir
+	calc_dir = proc_mkdir(PARENT_DIR, NULL);
+	if(!calc_dir) {
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
+
+	// arg1
+	arg1 = create_proc_entry(ARG1, 0666, calc_dir);
+	if(!arg1) {
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
+	arg1->write_proc = write_arg1;
+
+	// arg2
+	arg2 = create_proc_entry(ARG2, 0666, calc_dir);
+	if(!arg2) {
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
+	arg2->write_proc = write_arg2;
+
+	// operation
+	operation = create_proc_entry(OPERATION, 0666, calc_dir);
+	if(!operation) {
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
+	operation->write_proc = write_operation;
+
+	// result
+	result = create_proc_entry(RESULT, 0666, calc_dir);
+	if(!result) {
+		printk(KERN_INFO "Error creating proc entry");
+		return -ENOMEM;
+	}
+	result->read_proc = read_result;
+
+	printk(KERN_INFO "/proc/%s created\n", PARENT_DIR);
+	return 0;
+}
+
+void cleanup_module()
+{
+	remove_proc_entry(ARG1, NULL);
+	remove_proc_entry(ARG2, NULL);
+	remove_proc_entry(OPERATION, NULL);
+	remove_proc_entry(RESULT, NULL);
+	printk(KERN_INFO "/proc/%s removed\n", PARENT_DIR);
+}
+
+#endif
